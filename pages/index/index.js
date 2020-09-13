db1525eca2941bf6151a0a49daf675ebd7512c5c
Your Name <2242817829@qq.com>
2020-09-13 10:56:34 +0800

    小程序弹窗

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,48 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  handleShowToast() {
+    wx.showToast({
+      title: '加载中, Toast',
+      duration: 1000,
+      icon: 'loading',
+      success: function() {
+        console.log("展示弹窗成功")
+      },
+      fail: function() {
+        console.log("展示弹窗失败")
+      },
+      complete: function() {
+        console.log("完成函数的调用")
+      }
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+  handleShowModal() {
+    wx.showModal({
+      title: '你好, ShowModal',
+      content: '我是内容',
+      success: function(res) {
+        console.log(res)
+        if(res.cancel) {
+          console.log("用户点击了取消按钮")
+        } else {
+          console.log("用户点击了确定按钮")
         }
-      })
-    }
+      }
+    })
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  handleShowLoading() {
+    wx.showLoading({
+      title: 'Loading',
+      mask: true
+    })
+    // 必须手动调用hideLoading才会让loading消失
+    setTimeout(() => wx.hideLoading(), 1000)
+  },
+  handleShowAction() {
+    wx.showActionSheet({
+      itemList: ['相册', '拍照'],
+      success: function(res) {
+        console.log(res)
+      }
     })
   }
 })
